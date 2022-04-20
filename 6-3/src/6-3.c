1e08918d4450defd2962f5544385ea78e26f0122
cajlo83 <ceemm96@gmail.com>
2022-04-19 21:34:55 -0300

    lalala

--- 6-3/src/6-3.c
+++ 6-3/src/6-3.c
@@ -4,9 +4,19 @@
  Author      : Carlo Morici
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+ Description : Ejercicio 6-3:
+Pedirle al usuario su nombre y apellido (en variables separadas, una para el nombre y otra para el
+apellido). Ninguna de las dos variables se puede modificar.
+Debemos lograr guardar en una tercer variable el apellido y el nombre con el siguiente formato:
+Por ejemplo:
+
+Si el nombre es juan ignacio y el apellido es gOmEz, la salida debería ser:
+Gomez, Juan Ignacio
+
+============================================================================
  */
+
+#define
 
 #include <stdio.h>
 #include <stdlib.h>
